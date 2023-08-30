23341f7e7a7c60fe7bfb6f823007c1113ad36e18
Nayara Braga <nayara.mourabraga@gmail.com>
2023-08-30 20:57:57 -0300

    operacoes matematicas

---
+++ operacoes matematicas/script.js
@@ -0,0 +1,34 @@
+// Exercicio 1 - stage04
+
+let firstNumber = prompt ('Digite o primeiro número');
+let secondNumber = prompt ('Digite o segundo número');
+
+firstNumber = Number(firstNumber);
+secondNumber = Number(secondNumber);
+
+const sum = firstNumber + secondNumber;
+const sub = firstNumber - secondNumber;
+const multi = firstNumber * secondNumber;
+const div = firstNumber / secondNumber;
+const restDiv = firstNumber % secondNumber;
+
+
+alert ('A soma dos dois números é: ' + sum);
+alert ('A subtração dos dois números é: ' + sub);
+alert ('A multiplicação dos dois números é: ' + multi);
+alert ('A divisão dos dois números é: ' + div);
+alert ('O resto da divisão dos dois números é: ' + restDiv);
+
+if (sum % 2 === 0) {
+    alert ("O resultado da soma é par.")
+}
+else {
+    alert ("O resultado da soma é impar")
+}
+
+if (firstNumber === secondNumber) {
+    alert ("Os números inseridos são iguais.")
+}
+else {
+    alert ("Os números inseridos são diferentes.")
+}
